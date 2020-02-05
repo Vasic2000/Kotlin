00315e7f05b6559764c8586e696e5e975a7a9fba
Vasic2000 <Vasic2000@yandex.ru>
2020-02-05 17:50:28 +0300

    Repeat 50%

---
+++ app/src/main/java/su/vasic2000/kotlin/ui/note/NoteViewModel.kt
@@ -0,0 +1,19 @@
+package su.vasic2000.kotlin.ui.note
+
+import androidx.lifecycle.ViewModel
+import su.vasic2000.kotlin.data.NoteRepository
+import su.vasic2000.kotlin.data.entity.Note
+
+class NoteViewModel : ViewModel() {
+    private var pendingNote: Note? = null
+
+    fun save(note: Note){
+        pendingNote = note
+    }
+
+    override fun onCleared(){
+        pendingNote?.let {
+            NoteRepository.saveNote(it)
+        }
+    }
+}
